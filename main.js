30ee6a7d0d85d2f567e90205d84d25988d9d0841
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-20 15:58:53 -0500

    Try to make createGrid() function

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const gridContainer = document.querySelector("#grid-container");
 
+const gridSquare = document.createElement('div');
+gridSquare.classList.add('div');
+gridSquare.setAttribute('id', 'grid-suqare');
+gridContainer.appendChild(gridSquare);
 
+let i = 0;
+function createGrid() {
+    for(i = 0; i < 16; i++) {
+        if (i < 16) {
+        ++gridSquare;
+        console.log("Add another square");
+    } else {
+        console.log("16x16 grid complete");
+    }
+}
 
+};
 
 
