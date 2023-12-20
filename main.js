9474c62a8981de1b9e28aa61068353bc7948e426
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-20 16:01:31 -0500

    Comment out createGrid()

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ const gridContainer = document.querySelector("#grid-container");
 
 const gridSquare = document.createElement('div');
 gridSquare.classList.add('div');
-gridSquare.setAttribute('id', 'grid-suqare');
+gridSquare.textContent = "I'm a square";
+gridSquare.setAttribute('id', 'grid-square');
 gridContainer.appendChild(gridSquare);
 
-let i = 0;
-function createGrid() {
-    for(i = 0; i < 16; i++) {
-        if (i < 16) {
-        ++gridSquare;
-        console.log("Add another square");
-    } else {
-        console.log("16x16 grid complete");
-    }
-}
+// let i = 0;
+// function createGrid() {
+//     for(i = 0; i < 16; i++) {
+//         if (i < 16) {
+//         ++gridSquare;
+//         console.log("Add another square");
+//     } else {
+//         console.log("16x16 grid complete");
+//     }
+// }
 
-};
+// };
 
 
