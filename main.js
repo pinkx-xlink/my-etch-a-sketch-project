72ef38bc17b59dfaf5f414b670205f5a6d81a987
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-20 16:21:03 -0500

    Add flex to grid-container to make squares wrap

--- main.js
+++ main.js
@@ -11,7 +11,18 @@ const gridContainer = document.querySelector("#grid-container");
 let i = 0;
 function createGrid() {
     for (i = 0; i < 16; i++)
-    gridContainer.innerHTML+= `<div class="grid-squares" id="grid-squares"> I'm a square </div>`;
+    gridContainer.innerHTML+= 
+`<div class="grid-squares" id="grid-squares"> I'm a square </div>
+<style>
+#grid-squares {
+    background-color: pink;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    width: 120px;
+}
+</style>
+`;
     i++;
     console.log("added a square");
 };
