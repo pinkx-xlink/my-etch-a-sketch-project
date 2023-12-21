ff50cc916950f0d62ce8518b6e7e771523aab99b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-20 21:51:24 -0500

    Add same CSS from squares so new squares

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const gridContainer = document.querySelector("#grid-container");
 
-// let i = 0;
-// function createGrid() { 
-//     for (i = 0; i < 16; i++)
-//     gridContainer.innerHTML+= `<button class="grid-squares" id="grid-squares"></button>`;
-//     i++;
-// };
-// createGrid();
+ let i = 0;
+ function createGrid() { 
+     for (i = 0; i < 16; i++)
+     gridContainer.innerHTML+= `<button class="grid-squares" id="grid-squares"></button>`;
+     i++;
+ };
+createGrid();
 
 const gridSquares = document.querySelector('#grid-squares');
 
