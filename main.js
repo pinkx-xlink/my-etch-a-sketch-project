4bb0d2d6973130bd1650652a35d2a3d667923260
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-20 21:38:16 -0500

    Troubleshoot changeSize

--- main.js
+++ main.js
@@ -19,17 +19,17 @@ changeSizeBtn.textContent="Change grid size?";
 tools.appendChild(changeSizeBtn);
 
 changeSizeBtn.addEventListener('click', function changeSize() {
-    let newSize = prompt("How many squares?");
+    const newSize = prompt("How many squares?");
     console.log(`${newSize}`);
     
     function createNewGrid() { 
-        for (i = 0; i < newSize; i++) { 
+        for (i = 0; i < newSize; ++i) { 
         if (newSize > 100) {
             alert("Too much square, dude!!");
             break;
         } else if (newSize < 100) {
             gridContainer.innerHTML += `<button class="grid-squares" id="grid-squares"></button>`;
-            i++;
+            ++i;
         };  
         };
     };
