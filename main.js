ad2d2b8c24a4684d2444c3ea800296213aaeb0c5
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-20 20:10:42 -0500

    Undo adding the unnecessary {}, troubleshoot why it broke grid

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const gridContainer = document.querySelector("#grid-container");
 
 const gridSquares = document.createElement('button');
+gridSquares.addEventListener('click', function onClick(event) {
+    event.target.style.backgroundColor = 'salmon';
+});
+
 
 // gridSquares.classList.add('div');
 // gridSquares.textContent = "I'm a square";
@@ -11,10 +15,13 @@ const gridSquares = document.createElement('button');
 
 let i = 0;
 function createGrid() {
-    for (i = 0; i < 16; i++) {
-    gridContainer.innerHTML += `<div class="grid-squares" id="grid-squares"></div>`;
+    for (i = 0; i < 16; i++)
+    gridContainer.innerHTML+= 
+`<button class="grid-squares" id="grid-squares"></button>`;
     i++;
-    }
+    
 };
-
+gridSquares.addEventListener("click", function(e) {
+    this.classList.toggle("is-active");
+});
 createGrid();
