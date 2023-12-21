361cbdc31f62eb957c482adb044423f8dd25fb9c
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-20 22:04:11 -0500

    Init clearGrid() and call it in changeSize()

--- main.js
+++ main.js
@@ -18,17 +18,28 @@ changeSizeBtn.setAttribute('id', 'change-size-btn');
 changeSizeBtn.textContent="Change grid size?";
 tools.appendChild(changeSizeBtn);
 
+function clearGrid() {
+    if (gridSquares > 0) {
+        gridSquares === 0;
+        console.log("fresh canvas");
+    } else if (newGridSquares > 0) {
+        newGridSquares ===0;
+        console.log("fresh canvas");
+    }
+}
+
 changeSizeBtn.addEventListener('click', function changeSize() {
     const newSize = prompt("How many squares?");
     console.log(`${newSize}`);
-    
     function createNewGrid() { 
+        clearGrid();
+        let i = 0;
         for (i = 0; i < newSize;) { 
         if (newSize > 100) {
             alert("Too much square, dude!!");
             break;
         } else if (newSize < 100) {
-            const newGridSquares = document.createElement('div');
+            const newGridSquares = document.createElement('button');
             newGridSquares.setAttribute('id', 'new-grid-squares');
             gridContainer.appendChild(newGridSquares);
             ++i;
