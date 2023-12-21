ecc5094e82b435a8c7c59e2a141151279d4353dc
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-20 21:25:58 -0500

    Add if/else cases for if input > 100

--- main.js
+++ main.js
@@ -20,11 +20,17 @@ tools.appendChild(changeSizeBtn);
 changeSizeBtn.addEventListener('click', function changeSize() {
     let newSize = prompt("How many squares?");
     console.log(`${newSize}`);
+    
     function createNewGrid() { 
         for (i = 0; i < newSize; i++) { 
-        gridContainer.innerHTML += `<button class="grid-squares" id="grid-squares"></button>`;
-        i++;
+        if (newSize > 100) {
+            alert("Too much square, dude!!");
+            break;
+        } else if (newSize < 100) {
+            gridContainer.innerHTML += `<button class="grid-squares" id="grid-squares"></button>`;
+            i++;
+        };  
         };
     };
-   // createNewGrid();
+   createNewGrid();
 });
