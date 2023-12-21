19c0df4694474e4b06f9757f41117d849f3e10cb
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-20 20:29:40 -0500

    Init #tools and changeSizeBtn

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const gridContainer = document.querySelector("#grid-container");
 
-const gridSquares = document.querySelector('#grid-squares');
-
 let i = 0;
 function createGrid() { 
     for (i = 0; i < 16; i++)
-    gridContainer.innerHTML+= 
-`<button class="grid-squares" id="grid-squares"></button>`;
+    gridContainer.innerHTML+= `<button class="grid-squares" id="grid-squares"></button>`;
     i++;
 };
-
 createGrid();
+const gridSquares = document.querySelector('#grid-squares');
+
+const tools = document.querySelector("#tools");
+
+const changeSizeBtn = document.createElement('button');
+changeSizeBtn.classList.add('button');
+changeSizeBtn.setAttribute('id', 'change-size-btn');
+changeSizeBtn.textContent="Change grid size?";
+tools.appendChild(changeSizeBtn);
