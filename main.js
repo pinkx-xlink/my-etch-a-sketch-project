5c05ed3ce73343fe63cdbb1f002e8a5de38d3d60
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-20 20:19:22 -0500

    Remove commented code

--- main.js
+++ main.js
@@ -2,19 +2,12 @@ const gridContainer = document.querySelector("#grid-container");
 
 const gridSquares = document.querySelector('#grid-squares');
 
-// gridSquares.classList.add('div');
-// gridSquares.textContent = "I'm a square";
-// gridSquares.setAttribute('id', 'grid-squares');
-// gridContainer.appendChild(gridSquares);
-
-
 let i = 0;
 function createGrid() { 
     for (i = 0; i < 16; i++)
     gridContainer.innerHTML+= 
 `<button class="grid-squares" id="grid-squares"></button>`;
     i++;
-    
 };
 
 createGrid();
