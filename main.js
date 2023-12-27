dbcc63659267b459c63cb5cb013f2e6798410ba8
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-27 16:21:38 -0500

    Troubleshoot new Grid

--- main.js
+++ main.js
@@ -7,31 +7,28 @@ changeSizeBtn.setAttribute('id', 'change-size-btn');
 changeSizeBtn.textContent="Change grid size?";
 tools.appendChild(changeSizeBtn);
 
-function Grid() {
-
-    changeSizeBtn.addEventListener('click', function changeSize() {
-        const newSize = prompt("How many squares?");
-        console.log(`${newSize}`);
+// function Grid() {
+//     this.newGrid = function () {
+//         console.log("Congrats, we made a new grid using a constructor!")
+//     }
+// };
+changeSizeBtn.addEventListener('click', new Grid(+(prompt)) {
+    let newSize = prompt("How many squares?");
+    console.log(`${newSize}`);
+    let i = 0;
+    function createNewGrid() { 
         let i = 0;
-        function createNewGrid() { 
-            let i = 0;
-            for (i = 0; i < newSize;) { 
-            if (newSize > 100) {
-                alert("Too much square, dude!!");
-                break;
-            } else if (newSize < 100) {
-                const newGridSquares = document.createElement('button');
-                newGridSquares.setAttribute('id', 'new-grid-squares');
-                gridContainer.appendChild(newGridSquares);
-                ++i;
-            };  
-            };
+        for (i = 0; i < newSize;) { 
+        if (newSize > 100) {
+            alert("Too much square, dude!!");
+            break;
+        } else if (newSize < 100) {
+            const newGridSquares = document.createElement('button');
+            newGridSquares.setAttribute('id', 'new-grid-squares');
+            gridContainer.appendChild(newGridSquares);
+            ++i;
+        };  
         };
-       createNewGrid();
-    });
-    
-
-    this.newGrid = function () {
-        console.log("Congrats, we made a new grid using a constructor!")
-    }
-};
+    };
+   new Grid(+(prompt));
+});
