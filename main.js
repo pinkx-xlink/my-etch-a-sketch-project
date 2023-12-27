66908a46ac413eb97db5456d62a7c7929185c45d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-27 16:03:16 -0500

    Add back in tools and changeSizeBtn

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 const gridContainer = document.querySelector("#grid-container");
+const tools = document.querySelector("#tools");
+
+const changeSizeBtn = document.createElement('button');
+changeSizeBtn.classList.add('button');
+changeSizeBtn.setAttribute('id', 'change-size-btn');
+changeSizeBtn.textContent="Change grid size?";
+tools.appendChild(changeSizeBtn);
