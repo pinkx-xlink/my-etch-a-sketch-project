8ee55b6fe71e1bddbfcfab10bfb0c328bf741836
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-27 21:11:36 -0500

    Change console.log for grid measurements, troubleshoot Grid()

--- main.js
+++ main.js
@@ -30,11 +30,11 @@ function Grid() {
       if (gridSizeInput > 100) {
         alert("too many squares, dude!");
         console.log("New Grid not made - too many squares requested.");
-      } else if (gridSizeInput < 100) {
+      } else if (gridSizeInput < 100 && gridSizeInput < gridSize) {
         gridContainer.innerHTML += `<button>o</button>`;
         //gridContainer.appendChild(newGridSquares); Node issue
         console.log("Congrats, we made a new grid using a constructor!");
-        console.log(`${gridSizeInput}`); 
+        console.log(`Grid measurements: ${gridSizeInput} x ${gridSizeInput}`);
         ++i;
       };
     };
