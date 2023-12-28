c119efea43fa970095f6836d85eecc1ea71b24d5
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-27 20:21:16 -0500

    Remove gridSquares and troubleshoot why it broke the prompt

--- main.js
+++ main.js
@@ -8,18 +8,15 @@ changeSizeBtn.textContent="Change grid size?";
 tools.appendChild(changeSizeBtn);
 changeSizeBtn.addEventListener('click', new Grid());
 
-const gridSquares = document.createElement('button');
-gridSquares.classList.add('button');
-gridSquares.setAttribute('id', 'grid-squares');
-gridContainer.appendChild(gridSquares);
+
 
 function Grid() {
     const newGrid = function () {
         const gridSize = prompt("How many squares?");
         console.log("Congrats, we made a new grid using a constructor!");
         console.log(`${gridSize}`);
-        return newGrid;
-    return gridSize;
     }
+    return newGrid;
+    return gridSize;
 };
 
