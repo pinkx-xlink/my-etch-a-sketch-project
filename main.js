a01fea6e29d36ce9aabcaec8abf3a57122f750f3
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-31 14:04:58 -0500

    Remove unnecessary CSS for old code

--- main.js
+++ main.js
@@ -7,20 +7,23 @@ document.addEventListener("DOMContentLoaded", function() {
 const sizeBtn = document.querySelector("#size-btn");
 sizeBtn.addEventListener('click', getSize);
 
-function createGrid(size) {
-    let gridContainer = document.querySelector("#grid-container");
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-    for(i = 0; i < product; i++) {
-        let square = document.createElement('div');
-        square.style.backgroundColor = "pink";
-        gridContainer.insertAdjacentElement("beforeend", square);
-    }
-};
-
 function getSize() {
     const size = prompt("Please enter a number between 2 and 100");
     const product = (size * size);
     console.log(product);
-}
+
+    function createGrid(size) {
+        let gridContainer = document.querySelector("#grid-container");
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+        for(i = 0; i < product; i++) {
+            let square = document.createElement('div');
+            square.style.backgroundColor = "pink";
+            gridContainer.insertAdjacentElement("beforeend", square);
+        }
+    };
+};
+
+
+
