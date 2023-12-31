c111d34a02247ba0c36814f66604fcc16f898617
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-31 13:59:59 -0500

    Init getSize() and square

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    createGrid(16);
+    //createGrid(16);
+    //getSize();
     console.log(":P");
 });
 
 const sizeBtn = document.querySelector("#size-btn");
-const size = prompt("Please enter a number between 2 and 100");
+sizeBtn.addEventListener('click', getSize);
 
 function createGrid(size) {
     let gridContainer = document.querySelector("#grid-container");
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for(i = 0; i < product; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = "pink";
+        gridContainer.insertAdjacentElement("beforeend", square);
+    }
 };
+
+function getSize() {
+    const size = prompt("Please enter a number between 2 and 100");
+    const product = (size * size);
+    console.log(product);
+}
