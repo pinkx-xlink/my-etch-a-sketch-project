28b6f90663e22fd1a0f5cac9d69df2ba66973079
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-31 12:01:29 -0500

    Add grid template columns & rows

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const gridContainer = document.querySelector("#grid-container");
 
+function createGrid(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
