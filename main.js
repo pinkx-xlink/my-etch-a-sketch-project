4baf26a5f75416529a4737f31361132a6532cdb1
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-31 14:09:24 -0500

    Remove defer

--- main.js
+++ main.js
@@ -3,26 +3,28 @@ document.addEventListener("DOMContentLoaded", function() {
     //getSize();
     console.log(":P");
 });
-
+let gridContainer = document.querySelector("#grid-container");
 const sizeBtn = document.querySelector("#size-btn");
 sizeBtn.addEventListener('click', getSize);
 
 function getSize() {
     const size = prompt("Please enter a number between 2 and 100");
-    const product = (size * size);
+    return size;
+    let product = (size * size);
     console.log(product);
 
     function createGrid(size) {
-        let gridContainer = document.querySelector("#grid-container");
+        
         gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
-        for(i = 0; i < product; i++) {
+        for(i = 0; i < product; ++i) {
             let square = document.createElement('div');
             square.style.backgroundColor = "pink";
             gridContainer.insertAdjacentElement("beforeend", square);
         }
     };
+    createGrid();
 };
 
 
