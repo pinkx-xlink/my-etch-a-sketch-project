aeea038e649465e0342dd84339145d8624683189
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-31 13:49:41 -0500

    Delete unnecessary comments

--- main.js
+++ main.js
@@ -3,21 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log(":P");
 });
 
-let gridContainer = document.querySelector("#grid-container");
-
-//let size = document.querySelector('input');
 const sizeBtn = document.querySelector("#size-btn");
-sizeBtn.addEventListener('click', function () {
-    const size = prompt("Please enter a number between 2 and 100");
-    console.log(size);
-});
+const size = prompt("Please enter a number between 2 and 100");
+
 function createGrid(size) {
+    let gridContainer = document.querySelector("#grid-container");
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 };
-
-
-
-//sizeBtn.addEventListener('click', createGrid);
-//createGrid(10);
-
