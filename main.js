05a57408f81611894169dcdd4039b71d61e8b183
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-31 13:02:09 -0500

    Move createGrid() into sizeBtn eventListener

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ const sizeBtn = document.querySelector("#size-btn");
 sizeBtn.addEventListener('click', function () {
     let size = prompt("Please enter a number between 2 and 100");
     console.log(size);
+    function createGrid(size) {
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    }
 });
 
 
-function createGrid(size) {
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-}
+
+//sizeBtn.addEventListener('click', createGrid);
 //createGrid(10);
 
