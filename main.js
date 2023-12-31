74e43d532d04415fa137b6d33eb851ff8d3627cd
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-31 13:13:27 -0500

    Add lines 1-4

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    createGrid(16);
+    console.log(":P");
+});
+
 let gridContainer = document.querySelector("#grid-container");
 
 //let size = document.querySelector('input');
@@ -5,11 +10,11 @@ const sizeBtn = document.querySelector("#size-btn");
 sizeBtn.addEventListener('click', function () {
     const size = prompt("Please enter a number between 2 and 100");
     console.log(size);
-    function createGrid(size) {
-        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    };
 });
+function createGrid(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+};
 
 
 
