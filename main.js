13613b8daf25fb762e3c7e68460b6d9507cf0b78
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-31 12:42:06 -0500

    Add size prompt

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const gridContainer = document.querySelector("#grid-container");
 
+//let size = document.querySelector('input');
+let size = prompt("Please enter a number between 2 and 100");
+
 function createGrid(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
+createGrid(10);
+
