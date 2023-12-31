8ad63395b2d79f992d44c092ee2e05b1a67d48b0
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-31 12:53:00 -0500

    Init sizeBtn

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const gridContainer = document.querySelector("#grid-container");
 
 //let size = document.querySelector('input');
-let size = document.querySelector("#size-btn");
-size.addEventListener('click', function () {
-    prompt("Please enter a number between 2 and 100")}
-);
+const sizeBtn = document.querySelector("#size-btn");
+sizeBtn.addEventListener('click', function () {
+    let size = prompt("Please enter a number between 2 and 100");
+    console.log(size);
+});
 
 
 function createGrid(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
-createGrid(10);
+//createGrid(10);
 
