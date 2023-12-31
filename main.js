035c3b8625f59fa3a9f37cadb1d82fe7b196381c
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-12-31 13:10:03 -0500

    Redefine gridContainer with let instead of const

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const gridContainer = document.querySelector("#grid-container");
+let gridContainer = document.querySelector("#grid-container");
 
 //let size = document.querySelector('input');
 const sizeBtn = document.querySelector("#size-btn");
 sizeBtn.addEventListener('click', function () {
-    let size = prompt("Please enter a number between 2 and 100");
+    const size = prompt("Please enter a number between 2 and 100");
     console.log(size);
     function createGrid(size) {
         gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    }
+    };
 });
 
 
